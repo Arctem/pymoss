0bdeb0be7c41efc824158cf0d0e09a4316bc38fe
Russell White <rarctem@gmail.com>
2014-11-15 14:21:36 -0700

    Clean up code structure slightly.

--- moss.py
+++ moss.py
@@ -8,7 +8,6 @@ import sys
 prefix = ''
 error = []
 output = ''
-tmp_file = input('Assignment name? ')
 valid_archives = ['.tar.gz', '.tar', '.tgz', '.zip', '.7z']
 mode = 'python'
 #mode = 'c'
@@ -36,36 +35,16 @@ def valid_code_name(path):
     return False
 
 def extract(path, destination):
-    if f.endswith('.tar'):
+    if path.endswith('.tar'):
         return not os.system('tar -xf "{}" -C "{}"'.format(path, destination))
-    elif f.endswith('.tar.gz') or f.endswith('.tgz'):
+    elif path.endswith('.tar.gz') or path.endswith('.tgz'):
         return not os.system('tar -zxf "{}" -C "{}"'.format(path, destination))
-    elif f.endswith('.zip'):
+    elif path.endswith('.zip'):
         return not os.system('unzip "{}" -d "{}"'.format(path, destination))
-    elif f.endswith('.7z'):
+    elif path.endswith('.7z'):
         return not os.system('7za x "{}" "-o{}"'.format(path, destination))
     return False
 
-
-
-for f in os.listdir('.'):
-    if f.endswith('.zip'):
-        delete_all_tar = True
-        os.system('unzip "{}" -d {}'.format(f, tmp_file))
-
-for f in os.listdir(tmp_file):
-    if valid_archive_name(f):
-        #print(f)
-        last_name, first_name = f.split('_')[0].split('--', 1)
-        first_name = first_name.split('-')[0] #remove possible '-late'
-        dirname = prefix + last_name + '_' + first_name
-        os.system('mkdir "{}/{}"'.format(tmp_file, dirname))
-
-        f = tmp_file + '/' + f
-
-        if not extract(f, tmp_file + '/' + dirname):
-            print('File failed to extract: {}.'.format(f))
-            sys.exit()
 
 def gather_files(parent_path):
     valid_files = []
@@ -80,26 +59,52 @@ def gather_files(parent_path):
                     error.append('{} is not a text file. Check manually.'.format(f))
     return valid_files
 
-base_files = gather_files('base')
-student_files = gather_files(tmp_file)
+def main():
+    tmp_file = input('Assignment name? ')
 
-base = ''
-if len(base_files) > 0:
-	base = '-b ' + ' -b '.join(base_files)
-student = '"' + '" "'.join(student_files) + '"'
+    for f in os.listdir('.'):
+        if f.endswith('.zip'):
+            delete_all_tar = True
+            os.system('unzip "{}" -d {}'.format(f, tmp_file))
 
-#print output
-command = './moss -l c -d {base} {student}'.format(base = base, student = student)
-print('Uploading {} files.'.format(len(base_files) + len(student_files)))
-#print command
-os.system(command)
+    for f in os.listdir(tmp_file):
+        if valid_archive_name(f):
+            #print(f)
+            last_name, first_name = f.split('_')[0].split('--', 1)
+            first_name = first_name.split('-')[0] #remove possible '-late'
+            dirname = prefix + last_name + '_' + first_name
+            os.system('mkdir "{}/{}"'.format(tmp_file, dirname))
+
+            f = tmp_file + '/' + f
+
+            if not extract(f, tmp_file + '/' + dirname):
+                print('File failed to extract: {}.'.format(f))
+                sys.exit()
+
+    base_files = gather_files('base')
+    student_files = gather_files(tmp_file)
+
+    base = ''
+    if len(base_files) > 0:
+    	base = '-b ' + ' -b '.join(base_files)
+    student = '"' + '" "'.join(student_files) + '"'
+
+    #print output
+    command = './moss -l c -d {base} {student}'.format(base = base, student = student)
+    print('Uploading {} files.'.format(len(base_files) + len(student_files)))
+    #print command
+    os.system(command)
 
 
-input('Press enter to delete created files.')
-print('Deleting extra files.')
-os.system('rm -rf "{}"'.format(tmp_file))
+    input('Press enter to delete created files.')
+    print('Deleting extra files.')
+    os.system('rm -rf "{}"'.format(tmp_file))
 
-if len(error):
-    print('ERRORS:')
-    for e in error:
-        print(e)
+    if len(error):
+        print('ERRORS:')
+        for e in error:
+            print(e)
+
+
+if __name__ == '__main__':
+    main()
